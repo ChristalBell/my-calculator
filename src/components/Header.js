260fe86a7012df0cf19e6f81f2d2729b0ab64b83
Christal Bell <christalvictoriabell@gmail.com>
2023-07-23 21:56:34 -0700

    initial files

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div style={{ backgroundColor: "#3c4564" }}>
+      <h1 style={{ color: "#fefefc" }}>calc</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Keypad.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+const Keypad = () => {
+  const [count, setCount] = useState(0);
+  const one = () => {
+    setCount(1);
+  };
+};
+
+export default Keypad;
