7b27667d0cd6a54b2e6db375c9e7f960603eb45b
Christal Bell <christalvictoriabell@gmail.com>
2023-07-24 17:50:06 -0700

    buttons created but not working at all

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
+import { Button, ToggleButtonGroup } from "@mui/material";
 import React from "react";
 
 const Header = () => {
   return (
     <div style={{ backgroundColor: "#3c4564" }}>
       <h1 style={{ color: "#fefefc" }}>calc</h1>
+      <ToggleButtonGroup size="sm" variant="soft">
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+      </ToggleButtonGroup>
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,37 @@
-import { useState } from "react";
-const Keypad = () => {
-  const [count, setCount] = useState(0);
-  const one = () => {
-    setCount(1);
-  };
-};
+import React from "react";
+import { Button } from "@mui/material";
+function Keypad() {
+  return (
+    <div>
+      <div>
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
+        <Button>DEL</Button>
+      </div>
+      <div>
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button>+</Button>
+      </div>
+      <div>
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button>-</Button>
+      </div>
+      <div>
+        <Button>.</Button>
+        <Button>0</Button>
+        <Button>/</Button>
+      </div>
+      <div>
+        <Button>RESET</Button>
+        <Button>=</Button>
+      </div>
+    </div>
+  );
+}
 
 export default Keypad;
