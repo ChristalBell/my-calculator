7b69c79c6d65f7792dc1d420ad93ee2b497c684d
Christal Bell <christalvictoriabell@gmail.com>
2023-08-03 17:42:26 -0700

    + and - working

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,25 +1,18 @@
 import { Button } from "@mui/material";
-import React, { useReducer } from "react";
-const ACTIONS = {
-  ADD_DIGIT: "add-digit",
-  CHOOSE_OPERATION: "choose-operation",
-  CLEAR: "clear",
-  DELETE_DIGIT: "delete-digit",
-  EVALUATE: "evaluate",
-};
-function reducer(state, { type, payload }) {}
+import React, { useState } from "react";
+
 function Keypad() {
-  const [{ currentValue, previousValue, operation }, dispatch] = useReducer(
-    reducer,
-    {}
-  );
+  const [count, setCount] = useState(0);
+
+  function decrementCount() {
+    setCount((prevCount) => prevCount - 1);
+  }
+  function incrementCount() {
+    setCount((prevCount) => prevCount + 1);
+  }
   return (
     <div>
-      <div className="screen">
-        {previousValue}
-        {operation}
-        {currentValue}
-      </div>
+      <div> {count}</div>
       <div>
         <Button>7</Button>
         <Button>8</Button>
@@ -28,11 +21,11 @@ function Keypad() {
         <Button>4</Button>
         <Button>5</Button>
         <Button>6</Button>
-        <Button>+</Button>
+        <Button onClick={incrementCount}>+</Button>
         <Button>1</Button>
         <Button>2</Button>
         <Button>3</Button>
-        <Button>-</Button>
+        <Button onClick={decrementCount}>-</Button>
         <Button>.</Button>
         <Button>0</Button>
         <Button>/</Button>
