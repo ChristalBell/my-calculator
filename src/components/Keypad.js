c2678c525b65339090f14bb77617762f739260b2
Christal Bell <christalvictoriabell@gmail.com>
2023-08-03 17:25:39 -0700

    old try for keypad. Buttons but still no function

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,32 +1,42 @@
-import React from "react";
 import { Button } from "@mui/material";
+import React, { useReducer } from "react";
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+function reducer(state, { type, payload }) {}
 function Keypad() {
+  const [{ currentValue, previousValue, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   return (
     <div>
+      <div className="screen">
+        {previousValue}
+        {operation}
+        {currentValue}
+      </div>
       <div>
         <Button>7</Button>
         <Button>8</Button>
         <Button>9</Button>
         <Button>DEL</Button>
-      </div>
-      <div>
         <Button>4</Button>
         <Button>5</Button>
         <Button>6</Button>
         <Button>+</Button>
-      </div>
-      <div>
         <Button>1</Button>
         <Button>2</Button>
         <Button>3</Button>
         <Button>-</Button>
-      </div>
-      <div>
         <Button>.</Button>
         <Button>0</Button>
         <Button>/</Button>
-      </div>
-      <div>
+        <Button>X</Button>
         <Button>RESET</Button>
         <Button>=</Button>
       </div>
