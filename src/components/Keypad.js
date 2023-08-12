0493deea5910f542f15b28adf892b503b8d88d0f
Christal Bell <christalvictoriabell@gmail.com>
2023-08-11 22:17:48 -0700

    trying to add css

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 import React, { useReducer } from "react";
 import DigitButton from "./DigitButton";
 import OperationButton from "./OperationButton";
-
+import "./styles.css";
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
   CHOOSE_OPERATION: "choose-operation",
@@ -146,27 +146,28 @@ function Keypad() {
         </div>
         <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
-
-      <DigitButton digit="7" dispatch={dispatch} />
-      <DigitButton digit="8" dispatch={dispatch} />
-      <DigitButton digit="9" dispatch={dispatch} />
-      <Button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
-        DEL
-      </Button>
-      <DigitButton digit="4" dispatch={dispatch} />
-      <DigitButton digit="5" dispatch={dispatch} />
-      <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton operation="+" dispatch={dispatch} />
-      <DigitButton digit="3" dispatch={dispatch} />
-      <DigitButton digit="2" dispatch={dispatch} />
-      <DigitButton digit="1" dispatch={dispatch} />
-      <OperationButton operation="-" dispatch={dispatch} />
-      <DigitButton digit="." dispatch={dispatch} />
-      <DigitButton digit="0" dispatch={dispatch} />
-      <OperationButton operation="/" dispatch={dispatch} />
-      <OperationButton operation="x" dispatch={dispatch} />
-      <Button onClick={() => dispatch({ type: ACTIONS.CLEAR })}>RESET</Button>
-      <Button onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</Button>
+      <div className="bottom-screen">
+        <DigitButton digit="7" dispatch={dispatch} />
+        <DigitButton digit="8" dispatch={dispatch} />
+        <DigitButton digit="9" dispatch={dispatch} />
+        <Button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+          DEL
+        </Button>
+        <DigitButton digit="4" dispatch={dispatch} />
+        <DigitButton digit="5" dispatch={dispatch} />
+        <DigitButton digit="6" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
+        <DigitButton digit="3" dispatch={dispatch} />
+        <DigitButton digit="2" dispatch={dispatch} />
+        <DigitButton digit="1" dispatch={dispatch} />
+        <OperationButton operation="-" dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch} />
+        <DigitButton digit="0" dispatch={dispatch} />
+        <OperationButton operation="/" dispatch={dispatch} />
+        <OperationButton operation="x" dispatch={dispatch} />
+        <Button onClick={() => dispatch({ type: ACTIONS.CLEAR })}>RESET</Button>
+        <Button onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</Button>
+      </div>
     </div>
   );
 }
