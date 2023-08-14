bffa282858c76f9b8df81911b87998e99b9ebbab
Christal Bell <christalvictoriabell@gmail.com>
2023-08-13 19:49:00 -0700

    targeted DigitButton Customization

--- src/components/DigitButton.js
+++ src/components/DigitButton.js
@@ -1,11 +1,29 @@
-import { Button } from "@mui/material";
+import { Button, ThemeProvider, createTheme } from "@mui/material";
 import { ACTIONS } from "./Keypad";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "hsl(0, 0%, 90%)",
+    },
+    shadows: {
+      2: "hsl(0, 0%, 93%)",
+    },
+  },
+});
 export default function DigitButton({ dispatch, digit }) {
   return (
-    <Button
-      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
-    >
-      {digit}
-    </Button>
+    <ThemeProvider theme={theme}>
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        onClick={() =>
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })
+        }
+      >
+        {digit}
+      </Button>
+    </ThemeProvider>
   );
 }
