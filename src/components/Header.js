522a021e255fb381dbd784ffcdcae1df2f9bf445
Christal Bell <christalvictoriabell@gmail.com>
2023-08-13 20:50:54 -0700

    created slider  still needs more styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
-import { Switch } from "@mui/material";
+import { Slider } from "@mui/material";
 import React from "react";
 
 const Header = () => {
   return (
     <div>
       <h1>calc</h1>
-
       <div>
         <h2>theme</h2>
-        <Switch {...0} defaultChecked />
+        <Slider
+          sx={{
+            width: 50,
+            height: 15,
+          }}
+          defaultValue={1}
+          step={1}
+          marks
+          min={1}
+          max={3}
+        />
       </div>
     </div>
   );
